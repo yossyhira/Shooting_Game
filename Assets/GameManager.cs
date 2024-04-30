c317e895940b11ea0ab53108ae7f9d0f89555bf6
hirata <hirata@hogehoge.com>
2024-04-30 17:11:11 +0900

    update

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,6 +9,12 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     public int score;
     public Text score_text;
+    public Text Time_text;
+
+    private int time_count=0;
+    private float time=0;
+    private float time_reset=1.0f;
+
     void Start()
     {
         
@@ -16,6 +23,14 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        time+=Time.deltaTime;
+        if(time > time_reset){
+            time=0;
+            time_count++;
+        }
+
+        Time_text.text="Time "+time_count;
         score_text.text="Score: "+score;
+
     }
 }
